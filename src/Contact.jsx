38961249b1b5c6d6d8afb10fcd73ad137e60034e
JerzyJancewicz <jancewiczjerzy@gmail.com>
2023-01-13 22:51:17 +0100

    skonczony hover kontakt

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -2,10 +2,16 @@ import React from "react";
 
 function contact(){
     return (
-        <div class="contact">
-            <p id="contactP">
-              KONTAKT
-            </p>
+        <div class="contact1" >
+            <div class ="slide-out">
+                    <p class = "phoneNumber">85 652 34 50</p>
+                    <p class = "phoneNumber2">+48 798 047 870</p>
+            </div>
+            <div class="contact">
+                <p id="contactP">
+                    KONTAKT
+                </p>
+            </div>
         </div>
     )
 }
